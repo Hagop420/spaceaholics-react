76e669b69e77126b107ca23534fe018b79ac3fc1
Hagop Almadjian <hagop1isaboss@hotmail.com>
2024-03-12 20:30:23 -0700

    Spaceaholics-projecy-deployed

--- spaceaholics-revised/src/components/SeacrhImageAPI.tsx
+++ spaceaholics-revised/src/components/SeacrhImageAPI.tsx
@@ -335,7 +335,6 @@ export function SearchImageAPI() {
               onChange={handleChange}
               onKeyDown={handleKeyDown}
             />
-
             <button
               className="bg-blue-500 hover:bg-blue-700 text-white BL font-bold py-2 px-4 rounded transition-colors duration-300 ml-3 p-2"
               onClick={handleInputChange}
